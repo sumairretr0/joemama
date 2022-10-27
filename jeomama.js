b1233858b2bf13fccb700f5d3e52813dfcffd770
sumairretr0 <67121255+sumairretr0@users.noreply.github.com>
2022-10-27 13:06:07 +0500

    Add files via upload

---
+++ jeomama.js
@@ -0,0 +1,13 @@
+	// Own by Sumair Ahmed 
+    //
+    //  Proofing 
+
+
+    function devWho(){
+		const dev = "This Site is Developed By Sumair Ahmed"
+		const linkedDev = "https://www.linkedin.com/in/sumair-ahmed-709883210/"
+		const returnInfo = dev + "LinkedIn: " + linkedDev
+		console.log(dev + "LinkedIn: " + linkedDev)
+		return returnInfo;
+		
+	}
